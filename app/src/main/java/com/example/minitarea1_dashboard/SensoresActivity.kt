5aec08cfca20e8a9a40ac0a2cc05878cd9a27598
Maxxdm102 <valbri32@gmail.com>
2025-12-05 11:27:49 +0100

    Primer commit

--- app/src/main/java/com/example/minitarea1_dashboard/SensoresActivity.kt
+++ app/src/main/java/com/example/minitarea1_dashboard/SensoresActivity.kt
@@ -1,23 +1,11 @@
 package com.example.minitarea1_dashboard
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class SensoresActivity : AppCompatActivity() {
+class SensoresActivity : BaseMenuActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_sensores)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.sensores_root)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
         title = "Sensores"
     }
 }
